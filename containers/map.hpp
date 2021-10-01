e3735a6ff5d063e2c36ff5b06c483171270cc41b
jinbekim <jinbekim@student.42seoul.kr>
2021-10-01 10:57:30 +0900

    Add: pair & make_pair

---
+++ containers/map.hpp
@@ -0,0 +1,20 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+#include "util.h"
+namespace ft
+{
+class map
+{
+	public:
+		map(/* args*/);
+		map(const map &ref);
+		~map();
+		map& operator=(const map &ref);
+	private:
+		/* data */
+};
+
+}
+
+#endif
--- containers/utils.hpp
+++ containers/utils.hpp
@@ -146,8 +146,67 @@ template <class InputIterator1, class InputIterator2, class Compare>
 // 		return true;
 // 	}
 
-// std::pair
-// std::make_pair
+template< class T1, class T2 >
+struct pair
+{
+	typedef	T1	first_type;
+	typedef	T2	second_type;
+
+	first_type	first;
+	second_type	second;
+
+	pair()
+	:
+		first(),
+		second()
+	{}
+template<class U, class V>
+	pair (const pair<U,V>& pr)
+	:
+		first(pr.first),
+		second(pr.second)
+	{}
+	pair (const first_type& a, const second_type& b)
+	:
+		first(a),
+		second(b)
+	{}
+	pair& operator=( const pair& other )
+	{
+		if (*this == other)
+			return (*this);
+		this->first = other.first;
+		this->second = other.second;
+		return	(*this);
+	}
+};
+
+template <class T1,class T2>
+	pair<T1,T2> make_pair (T1 x, T2 y) { return (pair<T1,T2>(x,y)); }
+
+template <class T1, class T2>
+	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first==rhs.first && lhs.second==rhs.second; }
+
+template <class T1, class T2>
+	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs==rhs); }
+
+template <class T1, class T2>
+	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second); }
+
+template <class T1, class T2>
+	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(rhs<lhs); }
+
+template <class T1, class T2>
+	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return rhs<lhs; }
+
+template <class T1, class T2>
+	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs<rhs); }
 }
 
 #endif
